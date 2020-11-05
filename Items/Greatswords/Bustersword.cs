878b5094c6d00960271bad7a3833348e073a5a5a
Dark Hydrak <darkhydrak7@gmail.com>
2020-11-05 01:05:57 -0600

    Added placable and breakable modded ore!

---
+++ Items/Greatswords/Bustersword.cs
@@ -0,0 +1,73 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MonsterHunterWeapons.Items.Greatswords
+{
+    public class Bustersword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bustersword");
+            Tooltip.SetDefault("[c/0095ff:Weapon Class: Greatsword.] \n A crude iron great sword that, despite its mass-produced quality, packs quite a punch. Press <right> for a different attack!");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 10;
+            item.melee = true;
+            item.width = 1;
+            item.height = 1;
+            item.useTime = 50;
+            item.useAnimation = 50;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.knockBack = 20;
+            item.value = 10000;
+            item.rare = ItemRarityID.Blue;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+            item.useTurn = false;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.IronBar, 7);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        //Right click functionality
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                item.useStyle = ItemUseStyleID.SwingThrow;
+                item.useTime = 20;
+                item.useAnimation = 100;
+                item.damage = 30;
+            }
+            else
+            {
+                item.useStyle = ItemUseStyleID.SwingThrow;
+                item.useTime = 40;
+                item.useAnimation = 40;
+                item.damage = 10;
+            }
+            return base.CanUseItem(player);
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ Ore/Placeable/MachaliteOreDrop.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MonsterHunterWeapons.Ore.Placeable
+{
+    public class MachaliteOreDrop : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Machalite Ore");
+            Tooltip.SetDefault("A deep blue ore from a far away world.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTime = 10;
+            item.useAnimation = 15;
+            item.width = 12;
+            item.height = 12;
+            item.value = 2500;
+            item.rare = ItemRarityID.Blue;
+            item.autoReuse = true;
+            item.useTurn = true;
+            item.createTile = mod.TileType("MachaliteOre"); //Tile that will be created. Currently set to stone. Needs to be set to Modded ore.
+            item.maxStack = 999;
+            item.stack = 1;
+            item.consumable = true;
+        }
+    }
+}
